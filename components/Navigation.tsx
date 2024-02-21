490b17d02a0c95630fb2e03b9965f3c8876bab4c
Milad Joodi <winbeta.com@gmail.com>
2024-02-21 21:22:02 +0330

    conti...

---
+++ components/Navigation.tsx
@@ -0,0 +1,11 @@
+
+const Navigation = () => {
+    // const [first, setfirst] = useState(second)
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default Navigation;
